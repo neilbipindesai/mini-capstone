d79d38c552938437713e8cef2079c0b5a90d7fd0
YOUR NAME <YOUR@EMAIL.com>
2017-07-10 19:51:30 -0500

    Initial Comment

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
 	@products = Product.all
 	render "products.html.erb"
 	end
-end
+  def new
+    render "new.html.erb"
+  end
+  def create
+    recipe = Product.new(
+     name: params[:form_name],
+     image: params[:form_image],
+     price: params[:form_price],
+     description: params[:form_description]
+    )
+    recipe.save
+    render "create.html.erb"
+    end
+  end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "products_url" => "products#products_method"
+  get "/products/new" => "products#new"
+  post "/products" => "products#create"
 end
