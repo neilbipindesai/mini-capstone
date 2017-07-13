f822871d43580a7171b5481094f8e7885b8a2370
YOUR NAME <YOUR@EMAIL.com>
2017-07-12 19:47:22 -0500

    Added bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery (add for rails 5.1.0 or after)
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
