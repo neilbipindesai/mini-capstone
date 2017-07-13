a6517bc8564e67d88041265aa97c94a0a7ac763b
YOUR NAME <YOUR@EMAIL.com>
2017-07-12 19:16:37 -0500

    Made alot of changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,59 @@
 class ProductsController < ApplicationController
-	def products_method
-	@products = Product.all
-	render "products.html.erb"
-	end
+  def products_method
+    @products = Product.all
+    render "products.html.erb"
+  end
+
+  def index
+    @products = Product.all
+    render "index.html.erb"
+  end
+
   def new
     render "new.html.erb"
   end
+
   def create
-    recipe = Product.new(
+    product = Product.new(
      name: params[:form_name],
      image: params[:form_image],
      price: params[:form_price],
      description: params[:form_description]
-    )
-    recipe.save
-    render "create.html.erb"
-    end
+     )
+    product.save
+    flash[:success] = "Recipe successfully created!"
+    redirect_to "/products/#{product.id}"
   end
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "edit.html.erb"
+  end
+
+  def show
+    # product_id = params[:id]
+    @product = Product.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+  def update
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.name = params[:form_name]
+    @product.image = params[:form_image]
+    @product.price = params[:form_price]
+    @product.description = params[:form_description]
+    @product.save
+    flash[:success] = "Recipe successfully updated!"
+    redirect_to "/products/#{@product.id}"
+  end
+      
+  def destroy
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.destroy
+    flash[:warning] = "Product successfully destroyed!"
+    redirect_to "/products"
+    end
+end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get "products_url" => "products#products_method"
-  get "/products/new" => "products#new"
+  get "/products" => "products#index"
+  get "products/new" => "products#new"
   post "/products" => "products#create"
+  get "/products/:id" => "products#show"
+  get "/products/:id/edit" => "photos#edit"
+  patch "/photos/:id" => "photos#update"
+  delete "/photos/:id" => "photos#destroy"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
